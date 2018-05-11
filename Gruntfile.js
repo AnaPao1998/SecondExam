1c0a58a98aed71be341e170f34e5159ffe4946f5
Ana Paola Montero <anapao.montero@gmail.com>
2018-05-11 16:53:55 -0400

    CI-001: Multitask created

---
+++ Gruntfile.js
@@ -0,0 +1,59 @@
+module.exports = function (grunt) {
+	
+	var con = grunt.option('config'); 
+    var config = grunt.file.readJSON(con);
+
+grunt.registerTask('generateInd',function(){
+		grunt.file.copy('index.html', config.buildFolder + '/index.html',
+		 {
+		
+			process: function(files) {
+				return grunt.template.process(files,
+					{
+						data: {
+							appName: config.appName
+						}
+					});
+
+	   	 	}
+		});
+	});
+
+grunt.registerTask('generatePage1',function(){
+		grunt.file.copy('page1.html', config.buildFolder + '/page1.html',
+		 {
+		
+			process: function(files) {
+				return grunt.template.process(files,
+					{
+						data: {
+							pageOneName: config.pageOne
+						}
+					});
+
+	   	 	}
+		});
+	});
+
+grunt.registerTask('generatePage2',function(){
+		grunt.file.copy('page2.html', config.buildFolder + '/page2.html',
+		 {
+		
+			process: function(files) {
+				return grunt.template.process(files,
+					{
+						data: {
+							pageTwoName: config.pageTwo
+						}
+					});
+
+	   	 	}
+		});
+	});
+
+
+grunt.registerTask('multitask', 
+		['generateInd','generatePage1','generatePage2']);
+
+
+}
